81787bb75fee98659d08edd14940c535fff1cba4
Andy Wise <andyjwise@googlemail.com>
2014-04-16 19:45:06 +0100

    Update to mustache for templating

--- Website/prod/scripts/main.js
+++ Website/prod/scripts/main.js
@@ -1,4 +1,4 @@
-require(['planetData', 'timeline'], function(planetData, drawTimeline){
+require(['planetData', 'timeline','whatsnext', 'mustache'], function(planetData, drawTimeline, whatsnext, Mustache){
   $(document).ready(function(){
 
     drawTimeline();
@@ -187,15 +187,89 @@ require(['planetData', 'timeline'], function(planetData, drawTimeline){
         $('.timeline').slideDown();
       }
     }
+
+    function getMissionData(missionName){
+      for(var i = 0; i < whatsnext.length; i++){
+        if(whatsnext[i].name == missionName){
+          return whatsnext[i];
+        }
+      }
+    }
+
     function openInfoPanel(missionName){
       currentMission = missionName;
-      $('.topcontent-container').animate({height: '0px'});
-      if(missionName == "Mariner 4"){
-        $('#Mariner_4').animate({height: '285px'});
-      } else if( missionName == "Curiosity") {
-        $('#Curiosity').animate({height: '285px'});
-      }
-      render();
+      $('.topcontent-container').animate({height: '0px'}, function(){
+        var html = Mustache.to_html($('#infoPanelTemplate').html(), getMissionData(currentMission));
+        $('.topcontent-container').html(html);
+        $('#slider-images, #slider-factoids')
+         .anythingSlider({
+           // Appearance
+           theme               : "polished", // Theme name
+           mode                : "horiz",   // Set mode to "horizontal", "vertical" or "fade" (only first letter needed); replaces vertical option
+           expand              : false	,     // If true, the entire slider will expand to fit the parent element
+           resizeContents      : false,      // If true, solitary images/objects in the panel will expand to fit the viewport
+           aspectRatio         : null,      // Valid values: null, true, a float e.g. 1.5 (or as 3/2) or a ratio in a string e.g. '3:2'
+           showMultiple        : false,     // Set this value to a number and it will show that many slides at once
+           easing              : "swing",   // Anything other than "linear" or "swing" requires the easing plugin or jQuery UI
+
+           buildArrows         : false,      // If true, builds the forwards and backwards buttons
+           buildNavigation     : true,      // If true, builds a list of anchor links to link to each panel
+           buildStartStop      : false,      // If true, builds the start/stop button and adds slideshow functionality
+
+           appendForwardTo     : null,      // Append forward arrow to a HTML element (jQuery Object, selector or HTMLNode), if not null
+           appendBackTo        : null,      // Append back arrow to a HTML element (jQuery Object, selector or HTMLNode), if not null
+           appendControlsTo    : null,      // Append controls (navigation + start-stop) to a HTML element (jQuery Object, selector or HTMLNode), if not null
+           appendNavigationTo  : null,      // Append navigation buttons to a HTML element (jQuery Object, selector or HTMLNode), if not null
+           appendStartStopTo   : null,      // Append start-stop button to a HTML element (jQuery Object, selector or HTMLNode), if not null
+
+           toggleArrows        : false,     // If true, side navigation arrows will slide out on hovering & hide @ other times
+           toggleControls      : false,     // if true, slide in controls (navigation + play/stop button) on hover and slide change, hide @ other times
+
+           startText           : "Start",   // Start button text
+           stopText            : "Stop",    // Stop button text
+           forwardText         : "&raquo;", // Link text used to move the slider forward (hidden by CSS, replaced with arrow image)
+           backText            : "&laquo;", // Link text used to move the slider back (hidden by CSS, replace with arrow image)
+           tooltipClass        : "tooltip", // Class added to navigation & start/stop button (text copied to title if it is hidden by a negative text indent)
+
+           // Function
+           enableArrows        : true,      // if false, arrows will be visible, but not clickable.
+           enableNavigation    : false,      // if false, navigation links will still be visible, but not clickable.
+           enableStartStop     : true,      // if false, the play/stop button will still be visible, but not clickable. Previously "enablePlay"
+           enableKeyboard      : true,      // if false, keyboard arrow keys will not work for this slider.
+
+           // Navigation
+           startPanel          : 1,         // This sets the initial panel
+           changeBy            : 1,         // Amount to go forward or back when changing panels.
+           hashTags            : true,      // Should links change the hashtag in the URL?
+           infiniteSlides      : true,      // if false, the slider will not wrap & not clone any panels
+           navigationFormatter : null,      // Details at the top of the file on this use (advanced use)
+           navigationSize      : false,     // Set this to the maximum number of visible navigation tabs; false to disable
+
+           // Slideshow options
+           autoPlay            : true,     // If true, the slideshow will start running; replaces "startStopped" option
+           autoPlayLocked      : false,     // If true, user changing slides will not stop the slideshow
+           autoPlayDelayed     : false,     // If true, starting a slideshow will delay advancing slides; if false, the slider will immediately advance to the next slide when slideshow starts
+           pauseOnHover        : true,      // If true & the slideshow is active, the slideshow will pause on hover
+           stopAtEnd           : false,     // If true & the slideshow is active, the slideshow will stop on the last page. This also stops the rewind effect when infiniteSlides is false.
+           playRtl             : false,     // If true, the slideshow will move right-to-left
+
+           // Times
+           delay               : 5000,      // How long between slideshow transitions in AutoPlay mode (in milliseconds)
+           resumeDelay         : 15000,     // Resume slideshow after user interaction, only if autoplayLocked is true (in milliseconds).
+           animationTime       : 600,       // How long the slideshow transition takes (in milliseconds)
+           delayBeforeAnimate  : 0,         // How long to pause slide animation before going to the desired slide (used if you want your "out" FX to show).
+
+           // Interactivity
+           clickForwardArrow   : "click",         // Event used to activate forward arrow functionality (e.g. add jQuery mobile's "swiperight")
+           clickBackArrow      : "click",         // Event used to activate back arrow functionality (e.g. add jQuery mobile's "swipeleft")
+           clickControls       : "click focusin", // Events used to activate navigation control functionality
+           clickSlideshow      : "click",         // Event used to activate slideshow play/stop button
+           allowRapidChange    : false,           // If true, allow rapid changing of the active pane, instead of ignoring activity during animation
+
+         });
+        $('.topcontent-container').animate({height: '285px'})
+        render();
+      });
     }
 
     function clearUi(){
